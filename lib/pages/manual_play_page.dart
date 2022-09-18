9b4dd07da8e3615e4088e1068f907442f3beb44a
PTaati <ventotaasai@gmail.com>
2022-09-18 17:57:02 +0700

    create widget select sound asset

--- lib/pages/manual_play_page.dart
+++ lib/pages/manual_play_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ManualPlayPage extends StatefulWidget {
@@ -9,20 +8,91 @@ class ManualPlayPage extends StatefulWidget {
 }
 
 class _ManualPlayPageState extends State<ManualPlayPage> {
-  Widget _buildTitle(){
-    return Text('1 th room', style: TextStyle(fontSize: 50),);
+  final _soundAssetList = [
+    'assets/sounds/action/Alex Figueira - Longe.mp3',
+    'assets/sounds/action/Alex Figueira - Serious.mp3',
+    'assets/sounds/action/heros-time-paulo-kalazzi-main-version-01-51-2639.mp3',
+    'assets/sounds/action/Hogan Grip - Stance Gives You Balance.mp3',
+    'assets/sounds/action/Scott Holmes Music - Smart Tech.mp3',
+    'assets/sounds/action/Sergey Cheremisinov - Chernobyl.mp3',
+    'assets/sounds/action/Sergey Cheremisinov - Norilsk.mp3',
+  ];
+
+  String? _currentSoundAsset;
+
+  Widget _buildTitle() {
+    return Text(
+      '1 th room',
+      style: TextStyle(fontSize: 50),
+    );
   }
 
-  Widget _buildSoundTypeList(){
-    return Container();
-  }
-  
-  Widget _buildCurrentSound(){
+  Widget _buildSoundTypeList() {
     return Container();
   }
 
-  Widget _buildSelectingSoundList(){
+  Widget _buildCurrentSound() {
+    if (_currentSoundAsset != null) {
+      return Container(
+        child: Center(
+          child: Text(
+            _currentSoundAsset!,
+            style: TextStyle(fontSize: 20),
+            overflow: TextOverflow.clip,
+          ),
+        ),
+      );
+    }
+
     return Container();
+  }
+
+  Widget _buildSoundTile(String assetPath) {
+    const size = 200.0;
+    final soundName = assetPath.split('/').last;
+    return InkWell(
+      onTap: () {
+        setState(() {
+          _currentSoundAsset = assetPath;
+        });
+      },
+      child: Container(
+        margin: EdgeInsets.all(10),
+        height: size,
+        width: size,
+        decoration: BoxDecoration(
+          color: Colors.orange,
+          shape: BoxShape.circle,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Center(
+            child: Text(
+              soundName,
+              style: TextStyle(fontSize: 20),
+              overflow: TextOverflow.clip,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildSoundWidgets() {
+    return _soundAssetList
+        .map(
+          (assetFile) => _buildSoundTile(assetFile),
+        )
+        .toList();
+  }
+
+  Widget _buildSelectingSoundList() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: _buildSoundWidgets(),
+      ),
+    );
   }
 
   @override
